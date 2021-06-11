f6ca896e62447995304e03677fed9e2c6bf321b3
Thiago Felipe <thiago.felipe@tutanota.com>
2021-06-11 18:40:25 -0400

    Fazendo formulário de aplicação

--- semana10/labex/src/pages/ApplicationFormPage.js
+++ semana10/labex/src/pages/ApplicationFormPage.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { goToHomePage, goToTripsList } from "../routes";
+import useForm from "../hooks/useForm";
+import countries from "../utils/countries.json";
+import { applyTrip, listTrips } from "../api";
 
+/*eslint-disable-next-line max-lines-per-function*/
 export default function ApplicationFormPage() {
   const history = useHistory();
+  const { form, onChange } = useForm({
+    name:            "",
+    age:             "",
+    applicationText: "",
+    profession:      "",
+    country:         "",
+    trip:            ""
+  });
+  const [ trips, setTrips ] = useState([]);
+
+  async function listTripsFromApi() {
+    try {
+      setTrips((await listTrips()).data.trips);
+    } catch (error) {
+      alert(`Não foi possível pegar a lista de viagens\n${error.data.message}`);
+    }
+  }
+
+  useEffect(() => listTripsFromApi(), []);
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    try {
+      await applyTrip(form.trip, form);
+      alert("Aplicação feita com sucesso");
+    } catch (error) {
+      alert(`Erro na aplicação\n${error.data.message}`);
+    }
+  }
 
   return (
     <>
@@ -17,6 +51,71 @@ export default function ApplicationFormPage() {
         <button onClick={() => goToTripsList(history)}>
           Ver Viagens Disponíveis
         </button>
+        <form onSubmit={onSubmit}>
+          <label>Nome</label>
+          <input
+            type="text"
+            name="name"
+            value={form.name}
+            onChange={onChange}
+            minLength="3"
+            required
+          />
+          <label>Idade</label>
+          <input
+            type="number"
+            name="age"
+            value={form.age}
+            onChange={onChange}
+            min="18"
+            required
+          />
+          <label>Por Você É Um Ótimo Candidato</label>
+          <input
+            type="text"
+            name="applicationText"
+            value={form.applicationText}
+            onChange={onChange}
+            minLength="30"
+            required
+          />
+          <label>Profissão</label>
+          <input
+            type="text"
+            name="profession"
+            value={form.profession}
+            onChange={onChange}
+            minLength="10"
+            required
+          />
+          <label>País</label>
+          <select
+            name="country"
+            value={form.country}
+            onChange={onChange}
+            required
+          >
+            <option value="" disabled>Selecione Um País</option>
+            {countries.map((country) => (
+              <option key={country.numeric} value={country.name}>
+                {country.name}
+              </option>))}
+          </select>
+          <label>Viagem</label>
+          <select
+            name="trip"
+            value={form.trip}
+            onChange={onChange}
+            required
+          >
+            <option value="" disabled>Selecione Uma Viagem</option>
+            {trips.map((trip) => (
+              <option key={trip.id} value={trip.id}>
+                {`${trip.name} - ${trip.planet}`}
+              </option>))}
+          </select>
+          <button>Aplicar</button>
+        </form>
       </main>
     </>
   );
