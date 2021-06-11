5a46cdc1b96f11746b0478d87889ac327295baab
Thiago Felipe <thiago.felipe@tutanota.com>
2021-06-11 17:34:23 -0400

    Adicionando formulário de criar viagem

--- semana10/labex/src/components/TripCard.js
+++ semana10/labex/src/components/TripCard.js
@@ -6,7 +6,7 @@ export default function TripCard(props) {
       <h3>{props.trip.name}</h3>
       <p>{props.trip.description}</p>
       <p>{props.trip.planet}</p>
-      <p>{props.trip.date}</p>
+      <p>{new Date(props.trip.date).toLocaleDateString()}</p>
       <p>{`${props.trip.durationInDays} dias`}</p>
     </article>
   );
