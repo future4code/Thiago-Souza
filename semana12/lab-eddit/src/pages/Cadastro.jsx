9d50c703ff0f58f1a25f8351b65e34740a5ce6ad
Thiago Felipe <thiago.felipe@tutanota.com>
2021-06-23 16:56:03 -0400

    Fazendo parte estática da página Cadastro

--- semana12/lab-eddit/src/pages/Cadastro.jsx
+++ semana12/lab-eddit/src/pages/Cadastro.jsx
@@ -1,13 +1,64 @@
 import React from "react";
+import { useCoodinator } from "../hooks/useCoordinator";
+import useForm from "../hooks/useForm";
 
 export default function Cadastro() {
+  const { form, clearForm, handleChange } = useForm({
+    username: "",
+    email:    "",
+    passwor:  ""
+  });
+
+  const { goToLogin } = useCoodinator();
+
+  async function submitForm(event) {
+    event.preventDefault();
+    console.log(form);
+    clearForm();
+  }
+
   return (
     <>
       <header>
         Cadastro
       </header>
       <main className="Cadastro">
-        Cadastro
+        <form onSubmit={submitForm}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            name="username"
+            value={form.username}
+            onChange={handleChange}
+            placeholder="Digite Seu Username"
+            required
+          />
+          <label htmlFor="email">E-Mail</label>
+          <input
+            id="email"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            placeholder="Digite Seu E-Mail"
+            type="email"
+            required
+          />
+          <label htmlFor="password">Senha</label>
+          <input
+            id="password"
+            name="password"
+            value={form.password}
+            onChange={handleChange}
+            placeholder="Digite Sua Senha"
+            required
+          />
+          <button>
+            Cadastrar
+          </button>
+          <button type="button" onClick={goToLogin}>
+            Login
+          </button>
+        </form>
       </main>
     </>
   );
